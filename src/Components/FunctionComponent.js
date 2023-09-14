ee00816174120ddcb397443222da2e24e85d0ce4
adi1w <adianu1225@gmail.com>
2023-09-14 16:49:14 +0530

    new

---
+++ src/Components/FunctionComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './FunctionComponent.css'
+
+const FunctionComp =()=>{
+    return(
+        <>
+            <div className="functionDiv">
+                <h1>This is created using functional Component</h1>
+                <p className="paraCSSFunc">This is done using external CSS</p>
+                <p style={{color:"blue" , fontSize:"20px", fontWeight:"bold"}}>This is done using inline CSS</p>
+            </div>
+        </>
+    )
+}
+
+export default FunctionComp;
